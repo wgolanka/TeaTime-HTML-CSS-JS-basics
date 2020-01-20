c7c5331d50b7ee5e54f8b683d702eecda47f90a1
wgolanka <wiktoria.golanka@gmail.com>
2020-01-20 08:55:43 +0100

    Extract method to take params form url

--- tea-details/tea-details.js
+++ tea-details/tea-details.js
@@ -1,29 +1,19 @@
 $(document).ready(function () {
     console.log("ready - details screen");
 
-    const queryString = new Array();
     window.onload = function () {
+
+        let queryString = getPathParamsArray();
+
         let teaId = "id-fromurl";
-        if (queryString.length == 0) {
-            if (window.location.search.split('?').length > 1) {
-                const params = window.location.search.split('?')[1].split('&');
-                for (let i = 0; i < params.length; i++) {
-                    let key = params[i].split('=')[0];
-                    let value = decodeURIComponent(params[i].split('=')[1]);
-                    queryString[key] = value;
-                }
-            }
-        }
         if (queryString["id"] != null) {
             teaId = queryString["id"];
         }
-
 
         const url = "http://localhost:8080/teatime/tea/" + teaId;
 
         console.log("details final url: " + url);
         $.get(url, function (data) {
-            // console.log("Tea Loaded: " + data.harvestSeasons);
 
             const name = document.getElementById("name");
             name.innerText = data.name;
@@ -44,3 +34,19 @@ $(document).ready(function () {
     };
 
 });
+
+
+function getPathParamsArray() {
+    const queryString = new Array();
+    if (queryString.length == 0) {
+        if (window.location.search.split('?').length > 1) {
+            const params = window.location.search.split('?')[1].split('&');
+            for (let i = 0; i < params.length; i++) {
+                let key = params[i].split('=')[0];
+                let value = decodeURIComponent(params[i].split('=')[1]);
+                queryString[key] = value;
+            }
+        }
+    }
+    return queryString;
+}
