476218245d918ed26dbc4c088392424579b67f21
wgolanka <wiktoria.golanka@gmail.com>
2020-01-24 19:40:46 +0100

    Real accessories on tea-details screen

--- tea-details/tea-details.js
+++ tea-details/tea-details.js
@@ -40,7 +40,7 @@ $(document).ready(function () {
                 console.log("on click button edit tea details url: " + url)
             };
 
-            let urlDelete =  "http://127.0.0.1:8080/teatime/tea/delete" + idParam;
+            let urlDelete = "http://127.0.0.1:8080/teatime/tea/delete" + idParam;
 
             console.log(urlDelete);
 
@@ -55,7 +55,7 @@ $(document).ready(function () {
                         alert('Tea deleted successfully!');
                         location.href = url;
                     },
-                    error: function(){
+                    error: function () {
                         alert('Something went wrong :( Please try again later!');
                     }
                 });
@@ -68,6 +68,70 @@ $(document).ready(function () {
                 location.href = url;
                 console.log("on click button add remove accessories button redirect to url: " + url)
             };
+        });
+
+        let getTeaAccessoriesUrl = "http://localhost:8080/teatime/accessory/byTea/" + teaId;
+
+        $.get(getTeaAccessoriesUrl, function (data) {
+
+            if (data.length === 0) {
+                console.log("no accessories for this tea"); //TODO say it on site
+                return
+            }
+            console.log("User Accessories Loaded: " + data[0].id);
+
+            const userAccessories = document.getElementById("tea-accessories-ul");
+
+            const fragment = document.createDocumentFragment();
+
+            for (let i = 0; i < data.length; i++) {
+
+                let liCard = document.createElement("li");
+                liCard.className = "tiles-item";
+
+                let divCard = document.createElement("div");
+                divCard.className = "card";
+
+                let cardTitle = document.createElement("p");
+                cardTitle.className = "card-title";
+                cardTitle.innerText = data[i].name;
+
+                let img = document.createElement("img");
+                img.className = "tiles-img";
+                img.src = data[i].imageLink;
+                img.style = "width:25%";
+
+                let divCardTextContainer = document.createElement("div");
+                divCardTextContainer.className = "card-text-container";
+
+                let description = document.createElement("p");
+                description.innerText = "Description: " + data[i].description;
+
+                let isNecessary = document.createElement("p");
+                isNecessary.innerText = "Is necessary: " + data[i].isNecessary;
+
+                let priceRange = document.createElement("p");
+                priceRange.innerText = "Price Range: " + data[i].priceRange;
+
+                divCardTextContainer.appendChild(description);
+                divCardTextContainer.appendChild(isNecessary);
+                divCardTextContainer.appendChild(priceRange);
+                divCard.appendChild(cardTitle);
+                divCard.appendChild(img);
+                divCard.appendChild(divCardTextContainer);
+                divCard.id = data[i].id;
+
+                divCard.onclick = function () {
+                    let url = "../accessory-details/accessory-details.html" + "?id=" + encodeURIComponent(divCard.id);
+                    location.href = url;
+                    console.log("on click button accessory details url: " + url)
+                };
+
+                liCard.appendChild(divCard);
+                fragment.appendChild(liCard);
+            }
+
+            userAccessories.appendChild(fragment);
         });
     };
 
