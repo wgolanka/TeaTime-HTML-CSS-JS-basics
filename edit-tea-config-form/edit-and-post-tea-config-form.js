f59e9061c72313af5768f67096a86ec5b8932ae3
wgolanka <wiktoria.golanka@gmail.com>
2020-01-26 17:16:32 +0100

    Add validation to tea config edit screen

--- edit-tea-config-form/edit-and-post-tea-config-form.js
+++ edit-tea-config-form/edit-and-post-tea-config-form.js
@@ -1,3 +1,21 @@
+const brewingTime = document.getElementById('brewing-time');
+const ingredients = document.getElementById('ingredients');
+const drinkingTime = document.getElementById('drinking-time');
+const description = document.getElementById('description');
+
+const brewingTimeError = 'brewing-time-error';
+const ingredientsError = 'ingredients-error';
+const drinkingTimeError = 'drinking-time-error';
+const descError = 'desc-error';
+
+const errorColor = '#bb0018';
+const successValidationColor = '#b4b2b4';
+
+const blankFieldErrorMsg = " must be filled in";
+const specialCharactersErrorMsg = " can't contain special characters";
+
+let errorCount = 0;
+
 $(document).ready(function () {
     console.log("ready - edit tea screen");
 
@@ -15,7 +33,7 @@ $(document).ready(function () {
         console.log("details final url: " + url);
         $.get(url, function (data) {
 
-            if(data.brewingConfig != null){
+            if (data.brewingConfig != null) {
                 console.log("brewing config: " + data.brewingConfig);
                 const brewingTime = document.getElementById("brewing-time");
                 brewingTime.value = data.brewingConfig.brewingTime;
@@ -37,26 +55,27 @@ $(document).ready(function () {
 
 });
 
-//TODO add validation
-
-$('form').submit(function(e){
+$('form').submit(function (e) {
     e.preventDefault();
     const editTeaConfigDiv = document.getElementById("div-edit-config-tea");
-    let data = $( "#form-edit-config" ).serialize();
+    let data = $("#form-edit-config").serialize();
     data = data + "&teaId=" + encodeURIComponent(editTeaConfigDiv.getAttribute("teaId"));
     console.log("data: " + data);
 
-    $.ajax({
-        url: 'http://127.0.0.1:8080/teatime/tea/config/update',
-        type: 'PUT',
-        data: data,
-        success: function(data) {
-            alert('Tea config updated!');
-        },
-        error: function () {
-            alert("Something went wrong:( please try again later")
-        }
-    });
+    validateTeaConfig();
+    if (!errorCount) {
+        $.ajax({
+            url: 'http://127.0.0.1:8080/teatime/tea/config/update',
+            type: 'PUT',
+            data: data,
+            success: function (data) {
+                alert('Tea config updated!');
+            },
+            error: function () {
+                alert("Something went wrong:( please try again later")
+            }
+        });
+    }
 });
 
 
@@ -73,4 +92,116 @@ function getPathParamsArray() {
         }
     }
     return queryString;
+}
+
+
+function validateTeaConfig() {
+    errorCount = 0;
+    let isBrewingTimeOk = validateBrewingTime();
+    let isIngredientsOk = simpleValidate(ingredients, ingredientsError, "Ingredients");
+    let isDrinkingTimeOk = simpleValidate(drinkingTime, drinkingTimeError, "Drinking time");
+    let isDescOk = validateDescription();
+
+    scrollToFirstElementFailed(isBrewingTimeOk, isIngredientsOk, isDrinkingTimeOk, isDescOk);
+    showErrorSummary();
+}
+
+function simpleValidate(input, errorId, name) {
+    if (isFieldBlank(input)) {
+        errorOnInputField(input, errorId, name + blankFieldErrorMsg);
+        return false;
+    }
+    else if (containsSpecialCharacters(name)) {
+        errorOnInputField(input, errorId, name + specialCharactersErrorMsg);
+        return false;
+    }
+    else {
+        successValidation(input, errorId);
+        return true;
+    }
+}
+
+function isFieldBlank(input) {
+    return !input.value.trim().length;
+}
+
+function containsSpecialCharacters(input) {
+    let regexOnlyWordsAndNumber = /^[a-zA-Z0-9. ]+$/;
+    return !regexOnlyWordsAndNumber.exec(input.value);
+}
+
+function errorOnInputField(input, errorId, errorMsg) {
+    input.style.borderColor = errorColor;
+    document.getElementById(errorId).innerHTML = errorMsg;
+    input.placeholder = '';
+    console.warn(errorMsg);
+    errorCount++;
+}
+
+function successValidation(input, errorId) {
+    input.style.borderColor = successValidationColor;
+    document.getElementById(errorId).innerHTML = '';
+}
+
+function validateBrewingTime() {
+    if (isFieldBlank(brewingTime)) {
+        errorOnInputField(brewingTime, brewingTimeError, "Brewing time must be provided");
+        return false;
+    }
+
+    let parsedBrewingTime = parseFloat(brewingTime.value);
+
+    if (parsedBrewingTime < 0) {
+        errorOnInputField(brewingTime, brewingTimeError, "Brewing time can't be smaller than 0");
+        return false;
+    } else if (!isFieldBlank(brewingTime) && containsNonNumbers(brewingTime)) {
+        errorOnInputField(brewingTime, brewingTimeError, "Brewing time can't contain non numbers");
+        return false;
+    } else {
+        successValidation(brewingTime, brewingTimeError);
+        return true;
+    }
+}
+
+function containsNonNumbers(input) {
+    let regexOnlyNumbers = /^[0-9.]+$/;
+    return !regexOnlyNumbers.exec(input.value);
+}
+
+function validateDescription() {
+    if (isFieldBlank(description)) {
+        errorOnInputField(description, descError, "Description" + blankFieldErrorMsg);
+        return false;
+    }
+    else if (description.value.length > 200) {
+        errorOnInputField(description, descError, "Description maximum length is 200 characters");
+        return false;
+    }
+    else {
+        successValidation(description, descError);
+        return true;
+    }
+}
+
+function scrollToFirstElementFailed(isBrewingTime, isIngredientsOk, isDrinkingTime, isCaffeineContentOk, isDescOk) {
+    if (!isBrewingTime) {
+        brewingTime.scrollIntoView()
+    } else if (!isIngredientsOk) {
+        ingredients.scrollIntoView()
+    } else if (!isDrinkingTime) {
+        drinkingTime.scrollIntoView()
+    } else if (!isDescOk) {
+        description.scrollIntoView()
+    }
+}
+
+function showErrorSummary() {
+    let errorSummary = document.getElementById('error-summary');
+    errorSummary.style.color = errorColor;
+    if (errorCount > 0) {
+        errorSummary.innerHTML = errorCount === 1 ? errorCount + ' error to fix' : errorCount + " errors to fix";
+    } else {
+        errorSummary.innerHTML = 'Submitted successfully!';
+        errorSummary.style.color = '#49a02f';
+    }
 }
